aca8137f224f76a332c7079adb54b75d204bb95d
Heeaayoon <soohe14@gmail.com>
2025-06-09 14:59:13 +0900

    0609 리액트 환경설정

--- src/01/GroupText.jsx
+++ src/01/GroupText.jsx
@@ -1,7 +1,8 @@
+import { WiDayCloudy } from "react-icons/wi";
 function GroupText(){
     return(
-        <div className="text-2xl bg-green-900 text-white mb-2 p-2">
-            K-digital 2기
+        <div className="flex justify-center text-2xl bg-green-900 text-white mb-2 p-2">
+            K-digital 2기<WiDayCloudy />
         </div>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' //public 아래
 import './App.css'
 import GroupText from './01/GroupText';
 
