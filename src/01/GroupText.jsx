994f564a20adb738b3873d8ddab48c61216a7b80
Heeaayoon <soohe14@gmail.com>
2025-06-25 12:28:38 +0900

    리액트

--- src/01/GroupText.jsx
+++ src/01/GroupText.jsx
@@ -1,7 +1,7 @@
 import { WiDayCloudy } from "react-icons/wi";
 function GroupText(){
     return(
-        <div className="flex justify-center text-2xl bg-green-900 text-white mb-2 p-2">
+        <div className="flex justify-center items-center text-2xl bg-indigo-600 text-white mb-2 p-2">
             K-digital 2기<WiDayCloudy />
         </div>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
-import MyClock from './02/MyClock';
-import MyDiv from './03/MyDiv';
-import MyListItem from './04/MyListItem';
+import reactlogo from './assets/react.svg'
+import vitelogo from '/vite.svg'
+import GroupText from './01/GroupText'
+import MyClock from './02/MyClock'
+import MyListItem from './04/MyListItem'
+import MyList from './04/MyList'
 
 function App() {
-  return (
-    <MyListItem/>
+  return(
+    <div className = "w-full xl:w-8/10 h-screen mx-auto flex flex-col justify-start items-start">
+      <header className = "w-full min-h-20 bg-indigo-300
+                          flex justify-between items-center">
+            <div className="flex ml-10">
+            <img src={reactlogo} alt = "react"/>+
+            <img src='/vite.svg' alt = "vite"/>
+            <GroupText />
+          </div>
+      </header>
+      <main className="w-full flex-grow
+                        overflow-y-auto py-10
+                        flex flex-col justify-start items-center">
+        <MyList />                 
+      </main>
+      <footer className = "w-full min-h-20
+                          bg-black text-white
+                          flex justify-center items-center">
+            K-digital 2025 2기 Front-end
+
+      </footer>
+    </div>
   )
 }
 
